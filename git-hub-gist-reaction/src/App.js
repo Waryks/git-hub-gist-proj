632460615b5ac766cb243571ca2fbb592d9eb8dc
Alex Chimpan <62359477+Waryks@users.noreply.github.com>
2022-09-10 10:26:11 +0300

    Page layout v2.

--- git-hub-gist-reaction/src/App.js
+++ git-hub-gist-reaction/src/App.js
@@ -4,13 +4,15 @@ import React  from 'react';
 import HomePage from './pages/Home';
 import DetailPage from './pages/Detail';
 import ShowGistsPage from './pages/ShowGists';
+import SearchPage from './pages/Search';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path='/' element={<HomePage />} />
-        <Route path='/gists' element={<ShowGistsPage />} />
+        <Route path='/search' element={<SearchPage />} />
+        <Route path='/gists/:user' element={<ShowGistsPage />} />
         <Route path='/gist/:id' element={<DetailPage />} />
       </Routes>
     </BrowserRouter>
--- git-hub-gist-reaction/src/pages/ShowGists.js
+++ git-hub-gist-reaction/src/pages/ShowGists.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { useParams } from "react-router-dom";
 
 function ShowGistsPage(){
+    let {user} = useParams();
 
     return(
         
         <div>
             <h1>
-                Placeholder for showing gists list for a user
+                Placeholder for showing gists list for a user {user}
             </h1>
         </div>
     );
