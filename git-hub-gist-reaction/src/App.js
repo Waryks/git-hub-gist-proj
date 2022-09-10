8952983f73742e12eddd045ff598b1909ec8ff4f
Alex Chimpan <62359477+Waryks@users.noreply.github.com>
2022-09-10 14:44:53 +0300

    Changed home page

--- git-hub-gist-reaction/src/App.js
+++ git-hub-gist-reaction/src/App.js
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import React  from 'react';
 
-import HomePage from './pages/Home';
 import DetailPage from './pages/Detail';
 import ShowGistsPage from './pages/ShowGists';
 import SearchPage from './pages/Search';
@@ -9,8 +8,7 @@ import SearchPage from './pages/Search';
 function App() {
   return (
     <Routes>
-      <Route exact path='/' element={<HomePage />} />
-      <Route path='/search' element={<SearchPage />} />
+      <Route exact path='/' element={<SearchPage />} />
       <Route path='/gists/:user' element={<ShowGistsPage />}/>
       <Route path='/gist/:id' element={<DetailPage />} />
     </Routes>
